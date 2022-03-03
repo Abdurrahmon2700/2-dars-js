a02b8edb1ded387e923941af171f6a3ae27f5005
Abdurrahmon2700 <sabrin1998@inbox.ru>
2022-03-03 08:57:51 +0500

    no

---
+++ if.js
@@ -0,0 +1,310 @@
+// if 2
+// let a = 50;
+// let b = -89;
+// let c= 1;
+
+// if (a<b && b >c ) {
+//   console.log (a)
+// } 
+// else if (a>b || b<c ){
+//   console.log(c);
+// }
+  
+
+// if 3
+
+// let a = 10;
+// let b = -4;
+
+// let s;
+
+// if (a==b) {
+//   s=0;
+// }
+// else if (a>b){
+//   s=a;
+// }
+
+// console.log(s);
+
+// if 4 
+// domla ishlab berishdi
+
+
+// if 5
+// let x=-5;
+// let Fx;
+// if (x>0){
+//   Fx = 2*Math.sin(x);
+// }
+// else if (x<=0) {
+//   Fx = x-6; 
+// }
+// console.log(Fx);
+
+
+// if 6
+// let x = 6;
+// let Fx;
+// if (x<-2 || x>2) {
+//   Fx = 2*x;
+// }
+// else {
+//   Fx = 0+(-3*x)
+// }
+// console.log(Fx);
+
+
+
+// if 7
+// let x = 12;
+// let Fx;
+// if (x<=0){
+//   Fx = -x;
+// }
+// else if (0<x && x<2) {
+//   Fx = x**2;
+// }
+// else if(x >=2) {
+//   Fx=4;
+// }
+// console.log(Fx);
+
+
+//  if 8
+// let a = 3;
+// let b = 4;
+// let c = 5;
+// let s = c+b;
+// if (s>a) {
+//   console.log("Yes");
+// }
+// else {
+//   console.log("No");
+// }
+
+
+// if 9 
+
+// let temp = +prompt ("haroratni kiriting ")
+// if (temp<0) {
+//   console.log("Freezing")
+// }
+// else if (1 < temp && temp < 10) {
+//   console.log("Very Cold");
+// }
+// else if (11 < temp && temp < 20) {
+//   console.log("Cold");
+// }
+// else if (21 < temp && temp < 30) {
+//   console.log("Normal");
+// }
+// else if (31 < temp && temp < 40) {
+//   console.log("Hot");
+// }
+// else if (40 < temp) {
+//   console.log("Very Hot");
+// }
+
+//  if 10 
+
+// let son = +prompt ('son: ')
+// if (son>0 && son%2!=0) {
+//   console.log("musbat toq son");
+// }
+// else if (son>0 && son%2==0 ){
+//   console.log("musbat juft son");
+// }
+// else if (son<0 && son%2!=0) {
+//   console.log("manfiy toq son");
+// }
+// else if (son<0 && son%2==0) {
+//   console.log("manfiy juft son");
+// }
+// else if (son==0) {
+//   console.log("son 0 ga teng");
+// }
+
+// if 11 
+// let son = +prompt ("son kiriting ");
+// if (son>1 && son<100 && son %2==0) {
+//   console.log("Ikki xonali juft son");
+// }
+// else if (son>1 && son<100 && son %2!=0) {
+//   console.log("Ikki xonali toq son");
+// }
+// else if (son>99 && son<1000 && son %2==0) {
+//   console.log("Uch xonali juft son");
+// }
+// else if (son>99 && son<1000 && son %2!=0) {
+//   console.log("Uch xonali toq son");
+// }
+
+// if 12
+
+// let kub = +prompt ("kub sonini kiriting");
+// let s = kub**2;
+// console.log(s);
+
+
+// if 14
+// let n =27;
+// let a=n%10;
+// let d = (n-a)/10;
+// let s = a*10+d;
+// if (s<=n) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// if 15
+
+// let son = 45;
+// if (0<son && son<99) {
+//   console.log('2');
+// }
+// else if  (100<son && son<999) {
+//   console.log('3');
+// }else if (1000<son && son<10000) {
+//   console.log('4');
+// }
+
+//  for par
+
+// for 1
+
+// let n=5
+// let k=10
+
+// for (i=0; i<n; i++) {
+//   console.log(k);
+// }
+
+// for2
+
+// let a =10;
+// let b =20;
+// for (i=b; i<=b; i++) {
+//   console.log(i)
+// }
+
+// for3
+
+// 
+
+// for 4
+
+// let narx =5000;
+// let javob;
+// for (i=0; i<=10; i++) {
+//   javob = narx*i
+//   console.log(javob)
+// }
+
+// for 5
+
+// let cost = 2000;
+// let javob; 
+// for (i=0.1; i<=1; i+=0.1) {
+//   javob = cost*i;
+//   console.log(javob)
+// }
+
+// for 6
+// let cost = 2000;
+// let javob; 
+// for (i=1; i<=2; i+=0.2) {
+//   javob = cost*i;
+//   console.log(javob)
+// }
+
+// for 7
+// let son1 = 5;
+// let son2 = 15;
+
+// for (i=son1; i<=son2; i++) {
+//   console.log(i+i);
+// }
+
+// for 8 
+// let son1 = 5;
+// let son2 = 15;
+
+// for (i=son1; i<=son2; i++) {
+//   console.log(i*i);
+// }
+
+// for 9
+// let son1 = 5;
+// let son2 = 15;
+
+// for (i=son1; i<=son2; i++) {
+//   console.log(i**2);
+// }
+
+// for 10 
+// Ustoz ishlab berishdi
+// }
+
+// // for 11
+// let n=10;
+// for (i=0; i<=n; i++) {
+//   S=n**2+(n+i)**2;
+//   console.log(S)
+// }
+
+
+// for 12
+// let n =10;
+
+// for (i=1; i<=n; i+=0.1){
+//   S= i*i;
+//   console.log(S.toFixed(2));
+// }
+
+// for 16
+
+// let n =10;
+// let a=5;
+
+// for (i=0; i<n; i++) {
+//   b=a**i
+//   console.log(b);
+// }
+
+// for 17
+
+// let n =10;
+// let a=5;
+
+// for (i=0; i<n; i++) {
+//   b=a**i+a**i
+//   console.log(b);
+// }
+
+
+
+// for 18
+
+// 17-bilan bir xil ekan
+
+
+// 19 
+ 
+// let n = 5;
+
+// for (let i=1; i<=n; i++){
+//   let b=i*i
+//   console.log(b);
+// }
+
+// for 20 
+// let a=5;
+// let g = 1
+
+// for (let i=1; i<=a; i++) {
+//   g *=i
+//   console.log(g);
+// }
